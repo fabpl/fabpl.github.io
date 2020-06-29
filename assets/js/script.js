d3147bbac932b57896f0ca9041ff6675a9de686a
Fabrice Planchette <planchettefabrice@hotmail.com>
2020-06-29 12:34:27 +0200

    feat: new site

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+function toggleNavbar(id) {
+    document.getElementById(id).classList.toggle('hidden')
+    document.getElementById(id).classList.toggle('rounded')
+    document.getElementById(id).classList.toggle('shadow-lg')
+}
